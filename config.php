adba648fff967ebad1b2797510e819082ecf815e
Daniel Lockyer <daniel@daniellockyer.com>
2018-10-07 11:37:18 +0700

    Make clear file prompt default to true

--- config.php
+++ config.php
@@ -36,7 +36,7 @@ class Webgrind_Config extends Webgrind_MasterConfig {
     static $defaultFunctionPercentage = 90;
     static $defaultHideInternalFunctions = false;
 
-    static $promptConfirmationBeforeDeleteFiles = false;
+    static $promptConfirmationBeforeDeleteFiles = true;
 
     /**
      * Path to python executable
